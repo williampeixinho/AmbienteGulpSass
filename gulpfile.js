ea18a6ba0f471c840cfb3f37bc301d3c65784109
williampeixinho <159476623+williampeixinho@users.noreply.github.com>
2025-08-25 14:06:35 -0300

    add standalone gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -33,9 +33,15 @@ function serve() {
   watch(paths.html.src).on('change', browserSync.reload);
 }
 
+function watchFiles() {
+  watch(paths.styles.src, styles);
+}
+
 const build = series(styles);
 const dev = series(build, serve);
+const watchTask = series(build, watchFiles);
 
 exports.styles = styles;
 exports.build = build;
+exports.watch = watchTask;
 exports.default = dev;
